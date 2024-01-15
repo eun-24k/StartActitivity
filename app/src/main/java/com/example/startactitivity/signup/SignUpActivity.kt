187be735bfb9ebe04dbba14babd2d2b7e413d125
Euny Kim <euny4535@gmail.com>
2024-01-15 09:01:43 +0900

    주석 처리

--- app/src/main/java/com/example/startactitivity/signup/SignUpActivity.kt
+++ app/src/main/java/com/example/startactitivity/signup/SignUpActivity.kt
@@ -16,6 +16,7 @@ class SignUpActivity : AppCompatActivity() {
 
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
+    /** [0] 숙련 주차에 와서 처음 배운 바인딩을 이용해서 코드를 작성하였다. **/
     private lateinit var binding: ActivitySignupBinding
 
     private var accessPath: Int = 0
@@ -28,6 +29,7 @@ class SignUpActivity : AppCompatActivity() {
         arrayOf(binding.etName, binding.etId, binding.etPw)
     }
 
+    /** [1] 가장 먼저 액티비티 클래스와 뷰모델 클래스를 연결해 주어야 한다. **/
     private val viewModel by lazy {
         ViewModelProvider(this@SignUpActivity)[SignUpViewModel::class.java]
     }
@@ -39,6 +41,7 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(view)
 
         initView()
+        /** [3] 뷰모델 초기 함수를 만들어준다. **/
         initViewModel()
     }
 
@@ -47,12 +50,26 @@ class SignUpActivity : AppCompatActivity() {
             binding.btnSignup.text = "회원 수정"
             accessPath = 1
         }
+        /** [5] 가장 먼저 initView 함수가 실행 될것이거 그에 따라 아래 3개의 함수가 실행될 것이다. **/
         setTextChangedListener()
         setOnFocusChangedListener()
         onClickBtnSignUp()
     }
 
+    /** [4] 이 함수도 = with(viewModel)을 적어주어야 한다.
+     *      이 함수에서 뷰모델을 관찰할거라고 말해주는 부분인 것 같다.**/
     private fun initViewModel() = with(viewModel) {
+        /** [8] 어느 EditText에 입력값이 변경되었는지 전달 해 주었다.
+         *      이제 이 새로 입력되거나 지워진 값이 로그인 유효성 검사를 해주기 위해 setErrorMessage() 함수를 호출한다.
+         *      그리고 어느 EditText인지도 it을 통해 전달한다.
+         *      추가적으로 입력 정보가 모두 유효하면 로그인 버튼이 활성화되어야 하기 때문에
+         *      setConfrimButtenEnable() 함수도 실행해 준다.**/
+        editTextTextChange.observe(this@SignUpActivity) {
+            setErrorMessage(it)
+            setConfirmButtonEnable()
+        }
+        /** [12] SignUpErrorMessage 타입에서 문자열로 바꾸어준다.
+         *       EditText의 type과 그 문자열을  getNullIfValid() 함수를 호출하여 전달한다.**/
         nameErrorMessage.observe(this@SignUpActivity) {
             getNullIfValid("name", getString(it.message))
         }
@@ -63,6 +80,9 @@ class SignUpActivity : AppCompatActivity() {
             getNullIfValid("password", getString(it.message))
         }
 
+        /** [15] checkName에 저장되어있던 값을 EditText가 유효하지 않으면 띄울 수 있게 .error을 사용해준다.
+         *       그리고 나중에 이름,비밀번호, 아이디가 유효할 때 로그인 버튼을 활성화 해주기 위해 SignUpActivity에
+         *       있는 checkName 변수가 이름 에딧텍스트가 유효한 경우 null값을 가지게 해준다.**/
         checkName.observe(this@SignUpActivity) {
             binding.etName.error = it
             this@SignUpActivity.checkName = it
@@ -79,14 +99,12 @@ class SignUpActivity : AppCompatActivity() {
         enableConfrimButton.observe(this@SignUpActivity) {
             binding.btnSignup.isEnabled = it
         }
+        /** [18] [8]번과 동일**/
         editTextOnFocus.observe(this@SignUpActivity) {
             setErrorMessage(it)
             setConfirmButtonEnable()
         }
-        editTextTextChange.observe(this@SignUpActivity) {
-            setErrorMessage(it)
-            setConfirmButtonEnable()
-        }
+
 
     }
 
@@ -148,15 +166,21 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
+    /** [6] setTextChangedListener은 말 그대로 입력된 값이 변할 때마다 호출 되는 함수이다.
+     *      함수가 실행 되면 뷰모델에 있는 같은 이름의 함수를 호출한다.
+     *      이때 editTextArray를 넘겨준다.
+     *      EditTextArray는 이름/아이디/비밀번호에 입력된 값을 모두 가지고 있다.**/
     private fun setTextChangedListener() {
         viewModel.setTextChangedListener(editTextArray)
     }
-
+    /** [16] setOnFocusChangedListner은 EditText가 활성화 되어있는지 확인해주는 함수이다.
+     *       setTextChangedListener와 마찬가지로 editTextArray를 넘겨준다.**/
     private fun setOnFocusChangedListener() {
         viewModel.setOnFocusChangedListener(editTextArray)
 
     }
-
+    /** [10] 뷰모델에 있는 이름 유효성 검사 로직을 가진 함수를 호출해준다.
+     *       그리고 EditText에 적혀있는 입력값을 문자열로 바꾸어 전달한다.**/
     private fun getMessageValidName() {
         viewModel.setMessageValidName(binding.etName.text.toString())
     }
@@ -168,7 +192,9 @@ class SignUpActivity : AppCompatActivity() {
     private fun getMessageValidPassword() {
         viewModel.setMessageValidPassword(binding.etPw.text.toString())
     }
-
+    /** [13] 받은 값 그대로 뷰모델에 있는 setNullIfValid 함수로 바로 전해준다.
+     *       editText에 Error 함수를 사용하여 유효성 검사를 하고 싶은데 유효한 경우가 현재 Null이 아니고 empty String이어서
+     *       empty string을 null로 바꾸어주는 함수이다.**/
     private fun getNullIfValid(type: String, message: String) {
         viewModel.setNullIfValid(type, message)
     }
@@ -177,6 +203,8 @@ class SignUpActivity : AppCompatActivity() {
         viewModel.enableConfirmButton(checkName, checkId, checkPassword)
     }
 
+    /** [9] 어떤 EditText가 전달되었느냐에 따라 이름, 아이디 그리고 비밀번호의 유효성검사를 해준다.
+     * 이름 유효성 검사로 가보자!**/
     private fun setErrorMessage(type: String?) {
         when (type) {
             "name" -> getMessageValidName()
--- app/src/main/java/com/example/startactitivity/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/startactitivity/signup/SignUpViewModel.kt
@@ -9,6 +9,7 @@ import com.example.startactitivity.signup.SignUpValidExtension.includeAlphabetAn
 import com.example.startactitivity.signup.SignUpValidExtension.includeKorean
 import com.example.startactitivity.signup.SignUpValidExtension.includeSpecialCharacters
 
+/** [2] 뷰모델 클래스는 뷰모델을 상속받는다 (당연히...**/
 class SignUpViewModel : ViewModel() {
 
     private val _nameErrorMessage: MutableLiveData<SignUpErrorMessage> = MutableLiveData()
@@ -29,6 +30,10 @@ class SignUpViewModel : ViewModel() {
     val enableConfrimButton: LiveData<Boolean> get() = _enableConfirmButton
     val editTextOnFocus: LiveData<String?> get() = _editTextOnFocus
     val editTextTextChange: LiveData<String?> get() = _editTextTextChange
+    /** [11] 유효성 검사에 필요한 문자열들이 모두 SignUpErrorMessage 클래스에 들어있다.
+     *       여기서 문자열을 바로 가져오면 뷰모델에 뷰를 가져오는 것이므로 MVVM을 따르지 않는 것이다.
+     *       따라서 변수의 타입을 SignUpErrorMessage로 해주었다.
+     *       SignUpErrorMessage.NULL이 입력값이 유효한 경우이고 SignUpErrorMessage.NULL 은 비어있는 문자열이다.**/
     fun setMessageValidName(text: String) {
         _nameErrorMessage.value = when {
             text.isBlank() -> SignUpErrorMessage.EMPTY_NAME
@@ -53,10 +58,9 @@ class SignUpViewModel : ViewModel() {
         }
     }
 
-    fun enableConfirmButton() {
-
-    }
-
+    /** [14] 타입에 따라 각각 checkName, checkId, checkPassword에 들어가는 값을 지정해준다.
+     *       유효하면(isEmpty()인경우) null값을 부여해주고 아니면 원래 있었던 text를 전달한다.
+     *       이 유효성 검사 작업이 모든 EditText에 동일하게 적용되기 때문에 뷰모델 안에 editTextValidation 함수를 만들었다.**/
     fun setNullIfValid(type: String, text: String) {
         when (type) {
             "name" -> {
@@ -73,7 +77,7 @@ class SignUpViewModel : ViewModel() {
 
         }
     }
-
+    /** [15] 앞서 말했듯이 문자열이 비어있으면 null을 부여 아닌 경우에는 원래 있었던 문자열을 리턴한다.**/
     private fun editTextValidation(text:String):String? {
         val validate = when {
             text.isEmpty() -> null
@@ -88,7 +92,10 @@ class SignUpViewModel : ViewModel() {
             else -> false
         }
     }
-
+    /** [17] 에딧텍스트가 활성화 되어있을 경우엔ㄴ setTextChangedListener이 잘 작동할 것이기 때문에 아무런 작업을 해줄 필요가 없다.
+     *       근데 에딧텍스트가 비활성화 되었을 경우에 에딧텍스트에 들어 있는 값이 유효한지 마지막으로 한번 확인해 주어야한다.
+     *       따라서 hasFocus가 false일 때 어느 editText인지 구분하기 위해 전과 마찬가지로 type을 지정해준다.
+     *       만약 값이 유효하다면 이때도 null값을 가진다.**/
     fun setOnFocusChangedListener(editTextArray: Array<EditText>) {
         val type = arrayOf("name", "id", "password")
         for (i in type.indices) {
@@ -102,6 +109,12 @@ class SignUpViewModel : ViewModel() {
 
     }
 
+    /** [7] editTextArray에 들어있는 editText의 순서가 이름/아이디/비밀번호이기 때문에
+     *      동일한 인덱스를 사용하여 코드의 가독성을 높이기 위해 type이라는 배열에
+     *      "name, "id", "password"의 상수들을 저장해 주었다.
+     *      이 type이라는 배열은 앞으로도 같은 형식으로 계속 사용할 것이다.
+     *      이제 각각의 에딧텍스트가 변경 될때마다 어떤 에딧텍스트의 값이 변경되었는지 editTextTextChange라는 변수에 저장해 준다.
+     *      이 값을 이제 initViewModel() 함수에서 관찰해 줄 것이다.**/
     fun setTextChangedListener(editTextArray: Array<EditText>) {
         val type = arrayOf("name", "id", "password")
         for (i in type.indices) {
@@ -111,11 +124,5 @@ class SignUpViewModel : ViewModel() {
         }
     }
 
-    private fun setErrorMessage(type: String?,editTextArray: Array<EditText>) {
-        when (type) {
-            "name" -> setMessageValidName(editTextArray[0].text.toString())
-            "id" -> setMessageValidId(editTextArray[1].text.toString())
-            "password" -> setMessageValidPassword(editTextArray[2].text.toString())
-        }
-    }
+
 }
